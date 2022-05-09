710a0cbb0bcb40b868759c5bf22c92f7deb01305
Nirmal <nirmal.work01@gmail.com>
2022-05-09 08:11:14 +0545

    Final project need to check form is working or not

---
+++ form-handler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$phonenumber = $_POST['phonenumber'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+
+$email_from = 'info@greentechconcern.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body =  "User Name: $name.\n".
+                "User PhoneNumber: $phonenumber.\n".
+                "User Email: $email.\n".
+                "User Subject: $subject.\n".
+                "User Message: $message . \n" ;
+
+
+$to = 'nirmal.work01@gmail.com' ; 
+
+$headers = "From: $email_from \r\n" ;
+
+$headers .= "Reply-To : $email \r\n";
+
+
+mail($to , $email_subject , $email_body , $headers);
+
+header("Location: contact.html")
+?>
